b7b8d75a205e1f1d56e2c7f352bfaab703750e07
Billy Coover <billycoover@gmail.com>
2011-09-25 10:15:03 -0700

    new app

---
+++ SignalRTest/Chat.cs
@@ -0,0 +1,13 @@
+﻿using SignalR.Hubs;
+
+namespace SignalRTest
+{
+    public class Chat : Hub
+    {
+        public void Send(string message)
+        {
+            //Call the addMessage method on all clients.
+            Clients.addMessage(message);
+        }
+    }
+}
